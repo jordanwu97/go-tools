28b416866d5ce79067761d67582936eda0bca926
jordanw2 <jordanw2@illinois.edu>
2020-03-22 15:26:09 +0800

    added checkitem

--- ttl.go
+++ ttl.go
@@ -2,13 +2,15 @@ package gotools
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 // TTL time to live map. Don't use zero value.
 type TTL struct {
-	items   map[interface{}]*time.Timer
-	expired chan interface{}
+	items    map[interface{}]*time.Timer
+	itemsMtx sync.RWMutex
+	expired  chan interface{}
 }
 
 // Expired channel delivers items that expires
@@ -31,16 +33,31 @@ func (ttl *TTL) AddItem(item interface{}, expireIn time.Duration) {
 		panic(fmt.Errorf("expireIn must be larger than 1 nanosecond"))
 	}
 
+	ttl.itemsMtx.Lock()
+	defer ttl.itemsMtx.Unlock()
 	if t, exist := ttl.items[item]; exist {
+		// reset timer for item if it exists
 		t.Stop()
 		t.Reset(expireIn)
 	} else {
+		// start a new timer for item. when timer expires, stop it, delete it, and deliver it on expired channel
 		ttl.items[item] = time.AfterFunc(expireIn, func() {
+			ttl.itemsMtx.Lock()
 			ttl.items[item].Stop()
 			delete(ttl.items, item)
+			ttl.itemsMtx.Unlock()
 			ttl.expired <- item
 		})
 	}
+
+}
+
+// CheckItem returns whether the item is in the TTL
+func (ttl *TTL) CheckItem(item interface{}) bool {
+	ttl.itemsMtx.RLock()
+	defer ttl.itemsMtx.RUnlock()
+	_, exist := ttl.items[item]
+	return exist
 }
 
 // NewTTL instantiates the ttl map
--- ttl_test.go
+++ ttl_test.go
@@ -47,24 +47,36 @@ func TestTTL(t *testing.T) {
 	ttl.AddItem("2", 3*time.Second)
 	t2 := time.Now()
 
+	if !ttl.CheckItem("1") {
+		t.Fatalf("Item \"1\" should be in ttl")
+	}
+
+	if !ttl.CheckItem("2") {
+		t.Fatalf("Item \"1\" should be in ttl")
+	}
+
 	time.Sleep(time.Second)
 	ttl.AddItem("1", 5*time.Second)
 
 	thresh := 10 * time.Millisecond
 
 	if v := <-ttl.Expired(); v != "2" {
-		t.Fatalf("Incorrect value. Want %v, got %v", "2", v)
+		t.Fatalf("Incorrect value. Want %v, got %v", "\"2\"", v)
 	}
 	if delay := time.Since(t2); delay-3*time.Second > thresh {
 		t.Fatalf("Out of bound. Want %v, got %v", 3*time.Second, delay)
 	}
-	t.Log(time.Since(t2))
+	if ttl.CheckItem("2") {
+		t.Fatalf("Item \"2\" should not be in ttl")
+	}
 
 	if v := <-ttl.Expired(); v != "1" {
-		t.Fatalf("Incorrect value. Want %v, got %v", "1", v)
+		t.Fatalf("Incorrect value. Want %v, got %v", "\"1\"", v)
 	}
 	if delay := time.Since(t1); delay-6*time.Second > thresh {
 		t.Fatalf("Out of bound. Want %v, got %v", 6*time.Second, delay)
 	}
-	t.Log(time.Since(t2))
+	if ttl.CheckItem("1") {
+		t.Fatalf("Item \"1\" should not be in ttl")
+	}
 }
